db6b2279b5f059704911ee4c379610f58a98f9f1
Gyles Batara <gyles.batara@live.com>
2017-09-29 10:11:46 -0700

    Add different toppings

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,9 @@ Pizza.prototype.cost = function(size) {
     alert("It's physically impossible to eat a pizza that big. Please choose a size smaller than 24 inches.");
   } else if (size < 5) {
     alert("You'll still feel hungry! Choose a bigger pizza");
-  } else if (size >= 6 && size < 24) {
+  } else if (size >= 6) {
     return this.pizzaPrice - 6
-  } else if (size <= 24 && size < 30) {
+  } else if (size <= 29) {
     return this.pizzaPrice
   }
 }
@@ -31,18 +31,31 @@ $(document).ready(function() {
   var pizzaArray = [regular, vegan, beast, yawka, ditka];
 
   pizzaArray.forEach(function(pizza) {
-    $("select#type").append("<option value="+pizza.id+">"+pizza.pizzaStyle);
+    $("select#type").append("<option value="+pizza.id+">"+pizza.pizzaStyle+"</option>");
+  });
+
+  pizzaArray.forEach(function(topping) {
+    $("select#toppings").append("<option value="+topping.id+">"+topping.pizzaTopping+"</option>");
   });
 
   // Form Submit
   $("#formOne").submit(function(event) {
     event.preventDefault();
-
+    alert(this.pizzaStyle);
     var pizzaSelect = parseInt($("#type").val());
     var pizzaSize = $("input#size").val();
+    var price = 13;
+
+    pizzaArray.forEach(function(pizza) {
+      if (pizza.id === pizzaSelect) {
+        console.log(pizza.pizzaSize)
+        price = pizza.cost(size);
+      }
+    })
 
 
-    $("#price h2").append("You have chosen "+pizza.pizzaStyle+"Which comes with "+pizza.pizzaTopping);
+
+    $("#price h2").append("You have chosen "+this.pizzaStyle+"Which comes with "+this.pizzaTopping);
     $("#price h1").append("Your price will be "+price+".00")
 
   });
