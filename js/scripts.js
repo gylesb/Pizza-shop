61ab4766b48c2942aace4d49beb33f1280c47847
Daniel Cheng and Gyles Batara <daniel@>
2017-10-02 08:29:26 -0700

    Fix issue

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,9 @@ function Pizza(style, topping, price, id) {
 
 // Prototype
 Pizza.prototype.cost = function(size) {
-  if (size > 30) {
+  if (size >= 30) {
     alert("It's physically impossible to eat a pizza that big. Please choose a size smaller than 24 inches.");
-  } else if (size < 5) {
+  } else if (size <= 5) {
     alert("You'll still feel hungry! Choose a bigger pizza");
   } else if (size >= 6) {
     return this.pizzaPrice - 6
@@ -29,6 +29,7 @@ $(document).ready(function() {
   var beast = new Pizza("The BEAST", "Pepperoni, bacon, sausage, ham, eggs, and extra cheese", 13.00, 3);
   var yawka = new Pizza("The Yawka", "Cheese, and pepperoni", 13.00, 4);
   var ditka = new Pizza("The Ditka", "Sausage, pepperoni, bacon, and cheese", 13.00, 5);
+
 
   var pizzaArray = [regular, vegan, beast, yawka, ditka];
 
@@ -51,10 +52,10 @@ $(document).ready(function() {
       if (pizza.id === pizzaSelect) {
         console.log(pizza.pizzaStyle)
         console.log(pizza.pizzaPrice)
-        price = pizza.cost(size);
+        price = pizza.cost(pizzaSize);
       }
-    $("#price h2").text("You have chosen "+pizza.pizzaStyle+" with extra "+pizza.pizzaTopping+"! ");
-    $("#price h1").text("Your price will be $"+pizza.pizzaPrice+".00")
+    $("#price h2").text("You have chosen "+pizzaSelect+" with extra "+pizza.pizzaTopping+"! ");
+    $("#price h1").text("Your price will be $"+price+".00")
     });
   });
 });
